79e276b5108d6621684f7b01d5a28d6cb6292ee4
Dhakaltechnical <rupakdhakal98@gmail.com>
2021-03-23 20:11:44 +0545

    my studio website

---
+++ script.js
@@ -0,0 +1,9 @@
+
+//typinganimation script
+var typed = new Typed(".typing",{
+    strings:["Frame Photo","Cup Photo","Wedding Photo","T-shirt Photo","Passport-size Photo", "Flex Photo"],
+    typeSpeed: 100,
+    backSpeed:60,
+    loop:true
+});
+
